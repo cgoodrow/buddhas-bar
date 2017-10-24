e52857042b557046ed05b052a12fd0bbe8e53de4
Chris Goodrow <goodrow.chris4@gmail.com>
2017-10-24 16:39:29 -0400

    changed port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
      :address => "smtp.gmail.com",
-     :port => 587,
+     :port => 465,
      :domain => "gmail.com",
      :user_name => "goodrow.chris4@gmail.com",
      :password => "cg8102415210",
-     :authentication => "login",
+     :authentication => "plain",
      :enable_starttls_auto => true
  }
 
